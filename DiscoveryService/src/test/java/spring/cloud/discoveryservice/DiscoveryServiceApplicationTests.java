ebabd8d8eeb4195941a707cddc1acf7a3ce312f4
starseat <starseat3@gmail.com>
2021-07-29 17:38:30 +0900

    DiscoveryService 추가

---
+++ DiscoveryService/src/test/java/spring/cloud/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
