18104e25f1fcafd4b717d535d54398522e2fa999
starseat <jw.lee@bridgetec.co.kr>
2022-05-30 09:57:17 +0900

    최신화

--- FirstApiService/src/main/java/com/example/firstapiservice/controller/FirstApiServiceController.java
+++ FirstApiService/src/main/java/com/example/firstapiservice/controller/FirstApiServiceController.java
@@ -1,14 +1,25 @@
 package com.example.firstapiservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstApiServiceController {
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First Api Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        //System.out.println(header);
+        log.info(header);
+        return "Hello World in First Service.";
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # spring-cloud
 
 인프런 강의 소스 저장소
-> Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)
-> https://www.inflearn.com/스프링-클라우드-마이크로서비스
+> - Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)
+> - https://www.inflearn.com/스프링-클라우드-마이크로서비스
+
+
+> 참고
+https://wonit.tistory.com/489
 
 ---
 
